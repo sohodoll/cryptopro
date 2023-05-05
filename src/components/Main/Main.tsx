75f831a8b760def0dca6a7d6d1ea5656317f20ae
sohodoll <kovalevvtl@gmail.com>
2023-05-05 14:51:30 +0300

    feat: start search prompt calculation

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,6 +1,7 @@
 import { coinsList } from 'api/getCoins';
 import { useState, useEffect } from 'react';
 import { getCoinCoefficient } from 'api/getCoinCoefficient';
+import bg from 'assets/images/bitcoin-bg.png';
 import styles from './styles.module.css';
 
 type CoinType = {
@@ -47,15 +48,68 @@ export function Main() {
     setResult(parseFloat(calculationResult.toFixed(2)));
   };
 
+  const [searchPrompt, setSearchPrompt] = useState('');
+  const [searchAmount, setSearchAmount] = useState(0);
+
+  const onSearchButtonClick = () => {
+    const searchArray = searchPrompt.split(' ');
+    setSearchAmount(Number(searchArray[0]));
+  };
+
   return (
     <div className={styles.main}>
+      <div className="flex items-center justify-center">
+        <img className="w-80" src={bg} alt="bg" />
+        <form method="GET">
+          <div className="flex gap-4 items-center relative text-gray-600 focus-within:text-gray-400">
+            <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+              <button
+                type="submit"
+                className="p-1 focus:outline-none focus:shadow-outline"
+              >
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  viewBox="0 0 24 24"
+                  className="w-6 h-6"
+                >
+                  <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                </svg>
+              </button>
+            </span>
+            <input
+              type="search"
+              name="q"
+              className="border border-gray-800 py-2 text-sm text-white bg-gray-900 rounded-md pl-10 focus:outline-none focus:bg-white focus:text-gray-900"
+              placeholder="2 btc in usd"
+              autoComplete="off"
+              onChange={(e) => {
+                const { target } = e;
+                if (target instanceof HTMLInputElement) {
+                  setSearchPrompt(target.value);
+                }
+              }}
+            />
+            <button
+              type="button"
+              className="flex items-center h-10 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-1 px-4 border border-gray-400 rounded shadow"
+              onClick={onSearchButtonClick}
+            >
+              Calculate
+            </button>
+          </div>
+        </form>
+      </div>
       <div className="flex flex-col gap-5">
-        <form className="flex gap-5" action="none">
+        <form className="flex gap-5 text-xl items-center" action="none">
           <label className="" htmlFor="amount">
             Amount:
           </label>
           <input
-            className="border border-black"
+            className="border border-gray-300 rounded"
             type="number"
             name="amount"
             id="amount"
@@ -67,7 +121,7 @@ export function Main() {
             }}
           />
           <select
-            className="border border-black"
+            className="border border-black rounded"
             name="fromCurrency"
             id="fromCurrency"
             onChange={(e) => {
@@ -77,7 +131,7 @@ export function Main() {
             {coinsElements}
           </select>
           <select
-            className="border border-black"
+            className="border border-black rounded"
             name="intoCurrency"
             id="intoCurrency"
             onChange={(e) => {
@@ -87,16 +141,16 @@ export function Main() {
             {coinsElements}
           </select>
         </form>
-        <div className="flex gap-5">
+        <div className="text-xl font-bold flex gap-5 items-center justify-center">
           <div>Result:</div>
 
-          <span id="result" className="w-40 h-6 border border-black">
+          <span id="result" className="w-40">
             {result}
           </span>
           <button
             onClick={onCalculate}
             type="button"
-            className="border border-blue"
+            className="flex items-center h-10 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-1 px-4 border border-gray-400 rounded shadow"
           >
             Calculate
           </button>
